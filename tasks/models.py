cb21dfb153932ffb1e75f6fe0d755e22894387ce
Olexandr Hontarenko <sanya221138@gmail.com>
2023-12-11 15:38:00 +0200

    create models task and Tags also make migrations

---
+++ tasks/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField(blank=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField()
+    tags = models.ManyToManyField(Tags, related_name="tasks")
+
+    class Meta:
+        ordering = ("is_completed", "-datetime", )
